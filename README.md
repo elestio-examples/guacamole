d2806aea66978f0f2627b469beeef9debcf5dd11
jbenguira <z51biz@gmail.com>
2022-08-17 13:04:16 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# guacamole
-Deploy Apache Guacamole with CICD on Elestio
+# Guacamole on Elestio with CI/CD
+
+<a href="https://dash.elest.io/deploy?source=cicd&social=dockerCompose&url=https://github.com/elestio-examples/guacamole"><img src="deploy-on-elestio.png" alt="Deploy on Elest.io" width="180px" /></a>
+
+Example CI/CD pipeline showing how to deploy Guacamole on Elestio.
+
+# Once deployed ...
+
+You are now able to sign to the Admin UI here:
+    
+    https://[CI_CD_DOMAIN]/guacamole
+    login: root
+    password: (set in env var ADMIN_PASSWORD)
+
+
+# Quick start
+
+Login with admin credentials;
+Go to Settings -> Connections (pop-up menu at the top right);
+Create a new connection.
+
+Documentation: 
+https://guacamole.apache.org/doc/1.4.0/gug/
---
+++ scripts/postInstall.sh
@@ -0,0 +1,20 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+# Init guacamole database
+docker-compose up init-guac-db
+
+#Add new admin user and block default user guacadmin
+cat > ./mariadb/add.sql << EOF
+INSERT INTO guacamole_entity (entity_id, name, type) VALUES ('2', '${ADMIN_LOGIN}', 'USER');
+SET @salt = UNHEX(SHA2(UUID(), 256));
+INSERT INTO guacamole_user (entity_id, password_salt, password_hash, password_date) VALUES ('2', @salt, UNHEX(SHA2(CONCAT('${ADMIN_PASSWORD}', HEX(@salt)), 256)), NOW());
+INSERT INTO guacamole_user_permission (entity_id, affected_user_id, permission) VALUES ('2', '2', 'READ'), ('2', '2', 'UPDATE'), ('2', '2', 'ADMINISTER');
+INSERT INTO guacamole_system_permission (entity_id, permission) VALUES ('2', 'CREATE_CONNECTION'), ('2', 'CREATE_CONNECTION_GROUP'), ('2', 'CREATE_SHARING_PROFILE'),('2', 'CREATE_USER'),('2', 'CREATE_USER_GROUP'),('2', 'ADMINISTER');
+UPDATE guacamole_user SET disabled='1' WHERE user_id='1';
+GRANT ALL PRIVILEGES ON *.* TO 'guacamole_user'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION;
+EOF
+
+sleep 10s;
+
+docker-compose exec -T mariadb mysql -u root -p${MYSQL_ROOT_PASSWORD} guacamole_db < /var/lib/mysql/add.sql
