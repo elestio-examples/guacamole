73c7fe65fe3463601118405194bf5d5d68712c06
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-12 16:49:41 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,9 +1,9 @@
 mkdir -p ./init
-mkdir -p ./mariadb
+mkdir -p ./mysql
 mkdir -p ./drive
 mkdir -p ./record
 
 chown -R 1001:1001 ./init
-chown -R 1001:1001 ./mariadb
+chown -R 1001:1001 ./mysql
 chown -R 1001:1001 ./drive
 chown -R 1001:1001 ./record
