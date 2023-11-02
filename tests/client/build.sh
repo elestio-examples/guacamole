4e4322bdfe3ddce98707bdd563fac7bdcbc35a14
Prem chavhan <pchavhan@logiquad.com>
2023-11-02 22:10:30 +0530

    fix

--- tests/client/build.sh
+++ tests/client/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+sed -i "s~-DskipTests=false~-DskipTests=false -Drat.numUnapprovedLicenses=100~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/guacamole-client:latest | docker load
